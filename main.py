8de61d1202c3096d7fb64fc43a71ed73da6049e9
JHLEE <ljh_93@naver.com>
2019-06-27 19:24:18 +0900

    Implements crawler to sql insert query(executable)

--- main.py
+++ main.py
@@ -1,49 +1,8 @@
 import crawler
-import sqlite3
-import pymysql
 import psycopg2
 from datetime import datetime
 
-def get_conn():
-    host="db-central-ydd.cp97brx4lcga.ap-northeast-2.rds.amazonaws.com"
-    port=5432
-    dbname="db-central-ydd_development"
-    user="ydd"
-    password="a1274821"
-    conn = psycopg2.connect("dbname={} user={} host={} password={}".format(dbname,user,host,password))
-    return conn
-
-def check():
-    host="db-central-ydd.cp97brx4lcga.ap-northeast-2.rds.amazonaws.com"
-    port=5432
-    dbname="db-central-ydd_development"
-    user="ydd"
-    password="a1274821"
-    conn = psycopg2.connect("dbname={} user={} host={} password={}".format(dbname,user,host,password))
-    cur = conn.cursor()
-    quary = "Select * from Bulletins"
-    cur.execute(quary)
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-
-    cur = conn.cursor()
-    quary = "Select * from Posts"
-    cur.execute(quary)
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-
-    cur = conn.cursor()
-    quary = "Select * from crawl_lists"
-    cur.execute(quary)
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-
-
 def delete():
-    #conn = sqlite3.connect("db.sqlite3")
     conn=get_conn()
     cur = conn.cursor()
     quary = "DELETE FROM crawl_lists"
@@ -58,29 +17,23 @@ def delete():
     cur.execute(quary)
     conn.commit()
 
-   
-
-    
+def get_conn():
+    host="db-central-ydd.cp97brx4lcga.ap-northeast-2.rds.amazonaws.com"
+    port=5432
+    dbname="db-central-ydd_development"
+    user="ydd"
+    password="a1274821"
+    conn = psycopg2.connect("dbname={} user={} host={} password={}".format(dbname,user,host,password))
+    return conn
 
 def insert_into_bulletin(domain_title):
-    #conn = sqlite3.connect("db.sqlite3")
     conn=get_conn()
-
-    cur = conn.cursor()
-    quary = "Select MAX(id) from Bulletins"
-    cur.execute(quary)
-    rows = cur.fetchall()
-    if rows[0][0] is not None:
-        for row in rows:
-            max_bulletinid = row[0]+1
-    else:
-        max_bulletinid = 1
-
     cur = conn.cursor()
     quary = "Select title,id from Bulletins"
     cur.execute(quary)
     rows = cur.fetchall()
     is_joongbok = False
+    
     for row in rows:
        if domain_title == row[0]:
            is_joongbok = True
@@ -96,23 +49,14 @@ def insert_into_bulletin(domain_title):
         cur = conn.cursor()
         query = "Select id from Bulletins where title='"+domain_title+"'"
         cur.execute(query)
+        rows = cur.fetchall()
         for row in rows:
            return row[0]
 
 
 def insert_into_post(bulletin_id,site_title,url,select_rule):
-    #conn = sqlite3.connect("db.sqlite3")
-    conn=get_conn()
-    cur = conn.cursor()
-    quary = "Select MAX(id) from posts"
-    cur.execute(quary)
-    rows = cur.fetchall()
-    if rows[0][0] is not None:
-        for row in rows:
-            max_postid = row[0]+1
-    else:
-        max_postid = 1
 
+    conn=get_conn()
     cur = conn.cursor()
     quary = "Select title,id from posts"
     cur.execute(quary)
@@ -133,24 +77,14 @@ def insert_into_post(bulletin_id,site_title,url,select_rule):
         cur = conn.cursor()
         query = "Select id from Posts where title='"+site_title+"'"
         cur.execute(query)
+        rows = cur.fetchall()
         for row in rows:
             return row[0]
     else:
-        return max_postid-1
+        return 3
 
 def insert_into_crawl_list(post_id,title):
-#    conn = sqlite3.connect("db.sqlite3")
     conn=get_conn()
-    cur = conn.cursor()
-    quary = "Select MAX(id) from crawl_lists"
-    cur.execute(quary)
-    rows = cur.fetchall()
-    if rows[0][0] is not None:
-        for row in rows:
-            max_crawl_list_id = row[0]+1
-    else:
-        max_crawl_list_id = 1
-
     cur = conn.cursor()
     quary = "Select title,id from crawl_lists"
     cur.execute(quary)
@@ -159,7 +93,7 @@ def insert_into_crawl_list(post_id,title):
     for row in rows:
        if title == row[0]:
            is_joongbok = True
-           return row[1]
+           return
 
     if not is_joongbok:
         cur = conn.cursor()        
@@ -168,9 +102,7 @@ def insert_into_crawl_list(post_id,title):
         query = "insert into crawl_lists(post_id,title,created_at,updated_at) values ("+str(post_id)+",'"+title+"','"+str(now)+"','"+str(now)+"')"
         cur.execute(query)
         conn.commit()
-        return max_crawl_list_id
-    else:
-        return max_crawl_list_id-1
+        
 
 def insert_into_tables(rule, site_title, domain_title,url):
     bulletin_id = insert_into_bulletin(domain_title)
@@ -199,5 +131,3 @@ for elem in boards:
      insert_into_crawl_list(post_id,elem)
 
 insert_into_tables(rule,site_title, domain_title,url)
-
-check()
