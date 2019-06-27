9350f24b317ba638f831c1a7db5d7e1c31355571
EC2 Default User <ec2-user@ip-172-31-16-17.ap-northeast-1.compute.internal>
2019-06-27 18:26:04 +0000

    HOME : 우측 메뉴바 수정, 구독 DB 유효성 검사 등.

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -212,6 +212,7 @@ ActiveRecord::Schema.define(version: 20190627025633) do
 
   create_table "users", force: :cascade do |t|
     t.string "nickname"
+    t.integer "phone_number"
     t.boolean "admin", default: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
